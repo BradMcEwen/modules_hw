7e29ef1bf5a1715263f90dcaa057288e31d5bc91
BradMcEwen <mcewen.brad30@gmail.com>
2024-05-14 10:15:33 -0400

    module homework

---
+++ mood_responses.py
@@ -0,0 +1,15 @@
+def mood_happy():
+    print("i absolutely love to see it!")
+
+def mood_sad():
+    print("chin up soldier, tomorrow is a new day!")
+
+def mood_anxious():
+    print("take a step back and grab some water")
+
+def mood_excited():
+    print("LETS GOOOOO, TODAY'S A GOOD DAY!!")
+
+def mood_tired():
+    print("yeah, I can definitely relate. It is nap time!")
+
